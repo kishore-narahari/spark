f4872af6f600bef2b06cf166f6fe9f54d55a6181
kishore-narahari <anantakishore.n@gmail.com>
2016-03-17 23:51:54 -0700

    Update Spark.py

---
+++ spark/Spark.py
@@ -0,0 +1,42 @@
+# Databricks notebook source exported at Fri, 18 Mar 2016 06:50:27 UTC
+# MAGIC %md
+# MAGIC 
+# MAGIC # **Spark**
+# MAGIC Apache Spark is a fast and general-purpose cluster computing system.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Spark resources
+# MAGIC 
+# MAGIC There are a number of resources on the internet to learn Spark.  Here are some we recommend:
+# MAGIC 
+# MAGIC * [Spark Programming Guide](https://spark.apache.org/docs/latest/programming-guide.html)
+# MAGIC   * The latest official Apache Spark Programming Guide
+# MAGIC * [Spark Developer Resources](https://databricks.com/spark/developer-resources)
+# MAGIC   * See the Spark Developer Resources page on the Databricks website.
+# MAGIC * [Learning Spark Book](http://shop.oreilly.com/product/0636920028512.do) - Use our promo code: BWORM
+# MAGIC     * [Exercises from the Learning Spark book](/#workspace/databricks_guide/13 Sample Applications/4 Learning Spark Exercises/Overview) has been adapted for Databricks.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Tips for using Spark in Databricks:
+# MAGIC * [Intro Datasets](/#workspace/databricks_guide/05 Spark/1 Intro Datasets)
+# MAGIC   * Use the Databricks Spark Cluster UI to debug your Spark jobs. 
+# MAGIC * [Debugging](/#workspace/databricks_guide/05 Spark/2 Debugging)
+# MAGIC   * Use the Databricks Spark Cluster UI to debug your Spark jobs. 
+# MAGIC * [Caching](/#workspace/databricks_guide/05 Spark/3 Caching)
+# MAGIC   * Learn how to cache your data to increase your performance with Spark. 
+# MAGIC * [Scheduling Production Spark Jobs](#workspace/databricks_guide/02 Product Overview/05 Jobs)
+# MAGIC   * Use our Jobs feature to schedule your production Spark jobs.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Miscellaneous:
+# MAGIC * Run C/C++ Code in Spark -- in [[Python]](/#workspace/databricks_guide/05 Spark/4 Run C++ Code - py) or [[Scala]](/#workspace/databricks_guide/05 Spark/4 Run C++ Code - scala)
+# MAGIC * [Monte Carlo Simulation](/#workspace/databricks_guide/05 Spark/5 Monte Carlo Simulation)
